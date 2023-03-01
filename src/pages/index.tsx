7cb5c1f598d22709c8f468eb4363b87fb0cf68d0
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-01 13:18:46 -0500

    Add basic structure to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,29 @@ import Head from 'next/head'
 
 import styles from '@styles/HomePage.module.css'
 
-export default function HomePage() {
-  return (
-    <>
-      <Head>
-        <title>Done</title>
-        <meta name="description" content="Get stuff done." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import { Header } from '@components/header'
+import { Footer } from '@components/footer'
 
-      <main className={styles.main}>
-      </main>
-    </>
-  )
+export default function HomePage() {
+    return (
+        <>
+            <Head>
+                <title>Done</title>
+                <meta name="description" content="Get stuff done." />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+
+            <div className={styles.page}>
+                <Header />
+
+                <main className={styles.main}>
+                    <p>Main Content</p>
+                </main>
+
+                <Footer />
+            </div>
+        </>
+    )
 }
