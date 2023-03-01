b9ec8b7c70a305484d0ae4b2ee13ba1a909a711e
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-01 13:11:33 -0500

    Create Footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+
+import styles from 'Footer.module.css'
+
+export const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p className={styles.copyright}>Copyright © 2023 Parker Botsford</p>
+        </footer>
+    )
+};
