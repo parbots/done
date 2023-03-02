8c8ed320e3b12865c532c0dc599462b2f0a6960a
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-01 19:24:51 -0500

    Use Roboto Mono font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
+import { Roboto_Mono } from 'next/font/google'
+const robotoMono = Roboto_Mono({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <style jsx global>{`
+                html,
+                body {
+                    font-family: ${robotoMono.style.fontFamily};
+                }
+            `}</style>
+
+            <Component {...pageProps} />
+        </>
+    )
 }
