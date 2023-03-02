c11765c1aec5ad5af5697c114f4f03be3af9895e
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-02 13:51:31 -0500

    Add search and filter elements to ListMenu in Items module

--- src/modules/items/menu/ListMenu.tsx
+++ src/modules/items/menu/ListMenu.tsx
@@ -4,6 +4,8 @@ import { FormEvent } from 'react';
 
 import styles from './ListMenu.module.css'
 
+type Filter = 'all' | 'incomplete' | 'complete';
+
 type ListMenuProps = {
     addItem: (itemName: string) => void;
 };
@@ -24,13 +26,32 @@ export const ListMenu = (props: ListMenuProps) => {
 
         if (itemInputValue !== '' && itemInputValue[0] !== ' ') {
             props.addItem(itemInputValue);
+
             setItemInputValue('');
         }
     };
 
+    // TODO lift this state up
+    const [searchInputValue, setSearchInputValue] = useState('');
+
+    const handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+
+        setSearchInputValue(event.target.value);
+    };
+
+    // TODO lift this state up
+    const [filterValue, setFilterValue] = useState<Filter>('all');
+
+    const handleFilterRadioChange = (event: ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+
+        setFilterValue(event.target.value as Filter);
+    };
+
     return (
         <form onSubmit={handleFormSubmit} className={styles.menu}>
-            <section className={styles.addItemSection}>
+            <fieldset className={styles.addItemSection}>
                 <input
                     type="text"
                     name='itemInput'
@@ -40,7 +61,72 @@ export const ListMenu = (props: ListMenuProps) => {
                     className={styles.itemInput}
                 />
                 <button type="submit" className={styles.addItemButton}>Add</button>
-            </section>
+            </fieldset>
+
+            <fieldset className={styles.filterSection}>
+                <input
+                    type='text'
+                    name='searchInput'
+                    placeholder='Search...'
+                    value={searchInputValue}
+                    onChange={handleSearchInputChange}
+                    className={styles.searchInput}
+                />
+
+                <label htmlFor='filterRadioField' className={styles.filterRadioSectionLabel}>Show:</label>
+                <fieldset id='filterRadioField' className={styles.filterRadioSection}>
+                    <label
+                        htmlFor='show-all'
+                        data-selected={(filterValue === 'all').toString()}
+                        className={styles.filterLabel}
+                    >
+                        <input
+                            type='radio'
+                            name='show'
+                            id='show-all'
+                            value='all'
+                            checked={filterValue === 'all'}
+                            onChange={handleFilterRadioChange}
+                            className={styles.filterRadio}
+                        />
+                        All
+                    </label>
+
+                    <label
+                        htmlFor='show-incomplete'
+                        data-selected={(filterValue === 'incomplete').toString()}
+                        className={styles.filterLabel}
+                    >
+                        <input
+                            type='radio'
+                            name='show'
+                            id='show-incomplete'
+                            value='incomplete'
+                            checked={filterValue === 'incomplete'}
+                            onChange={handleFilterRadioChange}
+                            className={styles.filterRadio}
+                        />
+                        Doing
+                    </label>
+
+                    <label
+                        htmlFor='show-complete'
+                        data-selected={(filterValue === 'complete').toString()}
+                        className={styles.filterLabel}
+                    >
+                        <input
+                            type='radio'
+                            name='show'
+                            id='show-complete'
+                            value='complete'
+                            checked={filterValue === 'complete'}
+                            onChange={handleFilterRadioChange}
+                            className={styles.filterRadio}
+                        />
+                        Done
+                    </label>
+                </fieldset>
+            </fieldset>
         </form>
     );
 };
