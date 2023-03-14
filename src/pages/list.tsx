c378bbdf03184c47a75060200e4abe9e984c6952
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-14 01:34:09 -0400

    Add custom favicon and use on each page

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -213,13 +213,17 @@ export default function ListPage() {
         list.clearAllItems();
     };
 
+    // TODO only conditionally render components and elements within <header> and <main>
     if (error) return (
         <>
             <Head>
                 <title>Done</title>
                 <meta name='description' content='Get stuff done.' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <link rel='icon' href='/favicon.ico' />
+                <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+                <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+                <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+                <link rel='manifest' href='/site.webmanifest' />
             </Head>
 
             <div className={styles.page}>
@@ -247,7 +251,10 @@ export default function ListPage() {
                 <title>Done</title>
                 <meta name='description' content='Get stuff done.' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <link rel='icon' href='/favicon.ico' />
+                <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+                <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+                <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+                <link rel='manifest' href='/site.webmanifest' />
             </Head>
 
             <div className={styles.page}>
