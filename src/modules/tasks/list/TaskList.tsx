cf0003eb523a35f022ea134b20b50ac549357b25
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-20 08:14:58 -0400

    refactor: rename List component to TaskList

---
+++ src/modules/tasks/list/TaskList.tsx
@@ -0,0 +1,45 @@
+
+import styles from './List.module.css'
+
+import type { Task } from '@/types/task'
+
+import { TaskListItem } from './item'
+
+type TaskListProps = {
+    loading: boolean;
+    tasks: Task[];
+    removeTask: (selectedTask: Task) => void;
+    toggleTaskComplete: (selectedTask: Task) => void;
+    editTaskText: (selectedTask: Task, newTaskText: string) => void;
+};
+
+export const TaskList = (props: TaskListProps) => {
+
+    const taskListItems = props.tasks.map((task) => {
+        return (
+            <TaskListItem
+                key={task.id}
+                text={task.text}
+                complete={task.complete}
+                toggleSelfComplete={() => props.toggleTaskComplete(task)}
+                editSelfText={(newTaskText: string) => props.editTaskText(task, newTaskText)}
+                removeSelf={() => props.removeTask(task)}
+            />
+        );
+    })
+
+    if (props.loading) {
+        return (
+            <section className={styles.loadingSection}>
+                <p className={styles.loadingMessage}>Loading...</p>
+            </section>
+        );
+    }
+
+    return (
+        <ul className={styles.list}>
+            {taskListItems}
+        </ul>
+    );
+};
+
--- src/modules/tasks/list/index.ts
+++ src/modules/tasks/list/index.ts
@@ -1,1 +1,1 @@
-export { List } from './List'
+export { TaskList } from './TaskList'
