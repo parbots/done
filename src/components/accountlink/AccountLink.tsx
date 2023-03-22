3f841e10b496a498d73cb33ebbd0f31c69b7e160
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-22 10:50:42 -0400

    feat: add loading state and error handling to AccountLink component

--- src/components/accountlink/AccountLink.tsx
+++ src/components/accountlink/AccountLink.tsx
@@ -3,10 +3,30 @@ import styles from './AccountLink.module.css'
 
 import Link from 'next/link'
 
+import { useSessionContext } from '@supabase/auth-helpers-react'
+
+import * as Sentry from '@sentry/nextjs'
+
 export const AccountLink = () => {
+
+    const { isLoading, session, error } = useSessionContext();
+
+    if (error) {
+        Sentry.captureException(error);
+
+        return (
+            <p className={styles.error}>{error.message}</p>
+        );
+    };
+
+    if (isLoading || !session) {
+        return (
+            <p className={styles.loading}>Loading...</p>
+        );
+    }
+
     return (
-        <Link href='/account' className={styles.link}>
-        </Link>
+        <Link href='/account' className={styles.link}>Account</Link>
     );
 };
 
