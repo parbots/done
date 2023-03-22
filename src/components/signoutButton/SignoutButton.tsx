59f84214eeda0cc7dbbbd71593bd27a360f1ab07
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-22 11:13:20 -0400

    feat: create SignoutButton component

---
+++ src/components/signoutButton/SignoutButton.tsx
@@ -0,0 +1,8 @@
+
+import styles from './SignoutButton.module.css'
+
+export const SignoutButton = () => {
+    return (
+        <button className={styles.button}>Sign Out</button>
+    );
+};
---
+++ src/components/signoutButton/index.ts
@@ -0,0 +1,1 @@
+export { SignoutButton } from './SignoutButton'
