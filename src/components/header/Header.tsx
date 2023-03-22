6bb75a732219c18515a1436fc3e9286bd30182ab
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-22 11:57:49 -0400

    refactor: implement loading state and error handling in Header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,18 +3,55 @@ import styles from './Header.module.css'
 
 import { ReactNode } from 'react'
 
+import { useSessionContext } from '@supabase/auth-helpers-react'
+
+import { SigninLink } from '@/components/signinLink'
+import { SignupLink } from '@/components/signupLink'
+
 type HeaderProps = {
     children: ReactNode;
 };
 
 export const Header = (props: HeaderProps) => {
+
+    const { isLoading, session, error } = useSessionContext();
+
+    if (error) {
+        return (
+            <header className={styles.header}>
+                <h2 className={styles.title}>done</h2>
+
+                {error &&
+                    <nav className={styles.nav}>
+                        <p className={styles.error}>{error.message}</p>
+                    </nav>
+                }
+            </header>
+        );
+    }
+
     return (
         <header className={styles.header}>
-            <h2 className={styles.headerTitle}>done</h2>
+            <h2 className={styles.title}>done</h2>
 
-            <nav className={styles.headerNav}>
-                {props.children}
-            </nav>
+            {isLoading &&
+                <nav className={styles.nav}>
+                    <p className={styles.loading}>Loading...</p>
+                </nav>
+            }
+
+            {!isLoading && !session &&
+                <nav className={styles.nav}>
+                    <SigninLink />
+                    <SignupLink />
+                </nav>
+            }
+
+            {!isLoading && session &&
+                <nav className={styles.nav}>
+                    {props.children}
+                </nav>
+            }
         </header>
     );
 };
