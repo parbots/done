f8a8e8fec3e98ef295b84b2dbc48c7fb2a629397
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-22 11:19:59 -0400

    feat: handle signout and error handling in SignoutButton component

--- src/components/signoutButton/SignoutButton.tsx
+++ src/components/signoutButton/SignoutButton.tsx
@@ -1,8 +1,25 @@
 
 import styles from './SignoutButton.module.css'
 
+import { MouseEvent } from 'react'
+
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+
+import * as Sentry from '@sentry/nextjs'
+
 export const SignoutButton = () => {
+
+    const supabase = useSupabaseClient();
+
+    const handleSignout = async (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+
+        const { error } = await supabase.auth.signOut();
+
+        if (error) Sentry.captureException(error);
+    };
+
     return (
-        <button className={styles.button}>Sign Out</button>
+        <button onClick={handleSignout} className={styles.button}>Sign Out</button>
     );
 };
