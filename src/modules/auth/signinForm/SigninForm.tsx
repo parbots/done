e08e0cf3c3790039587149ba1cbb4c7b13ca5b62
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-23 11:17:01 -0400

    feat: implement supabase auth

--- src/modules/auth/signinForm/SigninForm.tsx
+++ src/modules/auth/signinForm/SigninForm.tsx
@@ -1,9 +1,15 @@
 
 import styles from './SigninForm.module.css'
 
-import { useId } from 'react'
+import { useRouter } from 'next/router'
+
+import { useId, useState } from 'react'
 
 import { useForm, SubmitHandler } from 'react-hook-form'
+
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+
+import * as Sentry from '@sentry/nextjs'
 
 type FormInputs = {
     email: string,
@@ -11,6 +17,31 @@ type FormInputs = {
 };
 
 export const SigninForm = () => {
+
+    const router = useRouter();
+
+    const supabase = useSupabaseClient();
+    const [authError, setAuthError] = useState<string | null>(null);
+
+    const signinUser = async (formEmail: string, formPassword: string) => {
+
+        setAuthError(null);
+
+        const { error } = await supabase.auth.signInWithPassword({
+            email: formEmail,
+            password: formPassword,
+        });
+
+        if (error) {
+            Sentry.captureException(error);
+
+            setAuthError('Unable to sign in, please confirm email and password are correct and try again.');
+
+            return
+        }
+
+        router.push('/list');
+    };
 
     const inputID = useId();
 
@@ -21,7 +52,7 @@ export const SigninForm = () => {
     const onSubmit: SubmitHandler<FormInputs> = (data, event) => {
         event?.preventDefault();
 
-        // Handle submit here
+        signinUser(data.email, data.password);
     };
 
     return (
@@ -35,11 +66,14 @@ export const SigninForm = () => {
                 <label htmlFor={'email' + inputID} className={styles.inputLabel}>Email:</label>
                 <input
                     {...register('email', {
-                        required: true,
+                        required: 'Please enter an email',
                     })}
                     id={'email' + inputID}
                     className={styles.input}
                 />
+                {errors.email &&
+                    <p className={styles.inputError}>{errors.email.message}</p>
+                }
             </fieldset>
 
             <fieldset
@@ -49,14 +83,21 @@ export const SigninForm = () => {
                 <label htmlFor={'password' + inputID} className={styles.inputLabel}>Password:</label>
                 <input
                     {...register('password', {
-                        required: true,
+                        required: 'Please enter a password',
                     })}
                     id={'password' + inputID}
                     className={styles.input}
                 />
+                {errors.password &&
+                    <p className={styles.inputError}>{errors.password.message}</p>
+                }
             </fieldset>
 
             <button type='submit' className={styles.submitButton}>Sign In</button>
+
+            {authError &&
+                <p className={styles.authError}>{authError}</p>
+            }
         </form>
     );
 };
