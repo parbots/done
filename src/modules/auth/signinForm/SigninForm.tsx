1cdc048207686fdbe939b6af09793066af345183
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-23 08:42:45 -0400

    feat: design basic SigninForm structure

--- src/modules/auth/signinForm/SigninForm.tsx
+++ src/modules/auth/signinForm/SigninForm.tsx
@@ -4,7 +4,19 @@ import styles from './SigninForm.module.css'
 export const SigninForm = () => {
     return (
         <form className={styles.form}>
-            <h1>Sign In</h1>
+            <h2 className={styles.title}>Sign In</h2>
+
+            <fieldset className={styles.inputFieldset}>
+                <label htmlFor='' className={styles.inputLabel}>Email:</label>
+                <input type='email' value='' className={styles.input} />
+            </fieldset>
+
+            <fieldset className={styles.inputFieldset}>
+                <label htmlFor='' className={styles.inputLabel}>Password:</label>
+                <input type='password' value='' className={styles.input} />
+            </fieldset>
+
+            <button type='submit'>Sign In</button>
         </form>
     );
 };
