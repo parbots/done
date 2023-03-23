0211d51984324285438cdb69806ed52b6b6a6d95
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-23 08:13:57 -0400

    feat: create SigninForm component in auth module

---
+++ src/modules/auth/signinForm/SigninForm.tsx
@@ -0,0 +1,10 @@
+
+import styles from './SigninForm.module.css'
+
+export const SigninForm = () => {
+    return (
+        <form className={styles.form}>
+            <h1>Sign In</h1>
+        </form>
+    );
+};
