3d0ee9abfc44866291024085e00b7a45e67ab58b
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-26 16:40:03 -0400

    feat(ChangePasswordForm): handle input state with react-hook-form

--- src/modules/auth/changePasswordForm/ChangePasswordForm.tsx
+++ src/modules/auth/changePasswordForm/ChangePasswordForm.tsx
@@ -3,17 +3,39 @@ import styles from './ChangePasswordForm.module.css'
 
 import { useId } from 'react'
 
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type FormInputs = {
+    currentPassword: string,
+    newPassword: string,
+    confirmNewPassword: string,
+};
+
 export const ChangePasswordForm = () => {
 
     const inputID = useId();
 
+    const { register, handleSubmit, formState: { errors } } = useForm<FormInputs>({
+        mode: 'onChange',
+        criteriaMode: 'all',
+    });
+
+    const onSubmit: SubmitHandler<FormInputs> = (data, event) => {
+        event?.preventDefault();
+
+        // Change user password
+    };
+
     return (
-        <form className={styles.form}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
             <h2 className={styles.title}>Change Password</h2>
 
             <fieldset className={styles.inputFieldset}>
                 <label htmlFor={'currentPassword' + inputID} className={styles.inputLabel}>Current Password:</label>
                 <input
+                    {...register('currentPassword', {
+                        required: true,
+                    })}
                     type='password'
                     id={'currentPassword' + inputID}
                     className={styles.input}
@@ -23,6 +45,9 @@ export const ChangePasswordForm = () => {
             <fieldset className={styles.inputFieldset}>
                 <label htmlFor={'newPassword' + inputID} className={styles.inputLabel}>New Password:</label>
                 <input
+                    {...register('newPassword', {
+                        required: true,
+                    })}
                     type='password'
                     id={'newPassword' + inputID}
                     className={styles.input}
@@ -32,6 +57,9 @@ export const ChangePasswordForm = () => {
             <fieldset className={styles.inputFieldset}>
                 <label htmlFor={'confirmNewPassword' + inputID} className={styles.inputLabel}>Confirm New Password:</label>
                 <input
+                    {...register('confirmNewPassword', {
+                        required: true,
+                    })}
                     type='password'
                     id={'confirmNewPassword' + inputID}
                     className={styles.input}
