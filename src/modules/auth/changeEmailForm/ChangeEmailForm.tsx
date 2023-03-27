60b5ab97d35934ab89a3971a905abb54ec9848d0
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-27 16:12:48 -0400

    feat(ChangeEmailForm): create component and basic structure

---
+++ src/modules/auth/changeEmailForm/ChangeEmailForm.tsx
@@ -0,0 +1,42 @@
+
+import styles from './ChangeEmailForm.module.css'
+
+import { useId } from 'react'
+
+export const ChangeEmailForm = () => {
+
+    const id = useId();
+
+    return (
+        <form className={styles.form}>
+            <h3 className={styles.title}>Change Email</h3>
+
+            <section className={styles.section}>
+                <label htmlFor={'currentEmail' + id} className={styles.sectionLabel}>Current Email:</label>
+                <p id={'currentEmail' + id} className={styles.sectionText}></p>
+            </section>
+
+            <fieldset className={styles.inputFieldset}>
+                <label htmlFor={'newEmail' + id} className={styles.inputLabel}></label>
+                <input
+                    type='text'
+                    id={'newEmail' + id}
+                    className={styles.input}
+                />
+                <p className={styles.error}></p>
+            </fieldset>
+
+            <fieldset className={styles.inputFieldset}>
+                <label htmlFor={'confirmNewEmail' + id} className={styles.inputLabel}></label>
+                <input
+                    type='text'
+                    id={'confirmNewEmail' + id}
+                    className={styles.input}
+                />
+                <p className={styles.error}></p>
+            </fieldset>
+
+            <button type='submit'>Change Email</button>
+        </form>
+    );
+};
---
+++ src/modules/auth/changeEmailForm/index.ts
@@ -0,0 +1,1 @@
+export { ChangeEmailForm } from './ChangeEmailForm'
