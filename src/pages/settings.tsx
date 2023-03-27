17010124e7fcc4845daca7c34f46f3ff0b1c3f0e
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-27 10:28:56 -0400

    refactor: rename account page to settings page and remove old styles

---
+++ src/pages/settings.tsx
@@ -0,0 +1,92 @@
+
+import styles from '@/styles/SettingsPage.module.css'
+
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import Link from 'next/link'
+
+import { useEffect } from 'react'
+
+import { useSessionContext } from '@supabase/auth-helpers-react'
+
+import { Header } from '@/components/header'
+import { SignoutButton } from '@/components/signoutButton'
+
+import { Footer } from '@/components/footer'
+
+export default function SettingsPage() {
+
+    const router = useRouter();
+
+    const { isLoading, session, error } = useSessionContext();
+
+    // If no user is signed in, redirect to homepage
+    useEffect(() => {
+        if (!isLoading && !session && !error) router.push('/');
+    }, [router, isLoading, session, error]);
+
+    // If there is an error loading the session, show error page
+    if (error) {
+        return (
+            <>
+                <Head>
+                    <title>Done</title>
+                    <meta name='description' content='Get stuff done.' />
+                    <meta name='viewport' content='width=device-width, initial-scale=1' />
+                    <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+                    <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+                    <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+                    <link rel='manifest' href='/site.webmanifest' />
+                </Head>
+
+                <div className={styles.page}>
+                    <Header>
+                        <Link href='/list' className={styles.headerLink}>My List</Link>
+                        <SignoutButton />
+                    </Header>
+
+                    <main className={styles.main}>
+                        <p className={styles.error}>{error.message}</p>
+                    </main>
+
+                    <Footer />
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Done</title>
+                <meta name='description' content='Get stuff done.' />
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+                <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+                <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+                <link rel='manifest' href='/site.webmanifest' />
+            </Head>
+
+            <div className={styles.page}>
+                <Header>
+                    <Link href='/list' className={styles.headerLink}>My List</Link>
+                    <SignoutButton />
+                </Header>
+
+                {isLoading &&
+                    <main className={styles.main}>
+                        <p className={styles.loading}>Loading...</p>
+                    </main>
+                }
+
+                {!isLoading && session &&
+                    <main className={styles.main}>
+                        <h1 className={styles.title}>Settings</h1>
+                    </main>
+                }
+
+                <Footer />
+            </div>
+        </>
+    );
+};
