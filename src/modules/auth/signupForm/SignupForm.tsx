cc180d39c4e76971c5838887c0a71df4f5c2115d
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-27 10:10:18 -0400

    refactor(SignupForm): refactor validation to only show message when errors are present

--- src/modules/auth/signupForm/SignupForm.tsx
+++ src/modules/auth/signupForm/SignupForm.tsx
@@ -21,9 +21,23 @@ export const SignupForm = () => {
 
     const router = useRouter();
 
-    const supabase = useSupabaseClient();
-    const [authError, setAuthError] = useState<string | null>(null);
+    const inputID = useId();
 
+    const { register, handleSubmit, formState: { errors } } = useForm<FormInputs>({
+        mode: 'onChange',
+        criteriaMode: 'all',
+    });
+
+    const onSubmit: SubmitHandler<FormInputs> = (data, event) => {
+        event?.preventDefault();
+
+        signupUser(data.email, data.confirmPassword);
+    };
+
+    const supabase = useSupabaseClient();
+
+    const [authError, setAuthError] = useState<string | null>(null);
+    const authErrorRef = useRef<HTMLParagraphElement | null>(null);
 
     const signupUser = async (formEmail: string, formPassword: string) => {
 
@@ -45,45 +59,19 @@ export const SignupForm = () => {
         router.push('/list');
     };
 
-    const authErrorRef = useRef<HTMLParagraphElement | null>(null);
-
     useEffect(() => {
         if (authErrorRef.current) authErrorRef.current.scrollIntoView();
     }, [authErrorRef]);
-
-    const inputID = useId();
-
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<FormInputs>({
-        mode: 'onChange',
-        criteriaMode: 'all',
-    });
-
-    const watchEmail = watch('email');
-    const watchPassword = watch('password');
-    const watchConfirmPassword = watch('confirmPassword');
-
-    const onSubmit: SubmitHandler<FormInputs> = (data, event) => {
-        event?.preventDefault();
-
-        signupUser(data.email, data.confirmPassword);
-    };
-
-    useEffect(() => {
-        console.log(errors);
-    }, [errors]);
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
             <h2 className={styles.title}>Sign Up</h2>
 
-            <fieldset
-                data-error={errors.email ? 'true' : 'false'}
-                className={styles.inputFieldset}
-            >
+            <fieldset data-error={errors.email ? 'true' : 'false'} className={styles.inputFieldset}>
                 <label htmlFor={'email' + inputID} className={styles.inputLabel}>Email:</label>
                 <input
                     {...register('email', {
-                        required: true,
+                        required: 'Required',
                         pattern: {
                             value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                             message: 'Must be a valid email address',
@@ -95,34 +83,22 @@ export const SignupForm = () => {
                     className={styles.input}
                 />
 
-                {!watchEmail &&
-                    <p
-                        className={styles.inputError}
-                    >
-                        Must be a valid email address
-                    </p>
-                }
-                {watchEmail &&
-                    <p
-                        data-error={errors.email ? 'true' : 'false'}
-                        role={errors.email ? 'alert' : ''}
-                        className={styles.inputError}
-                    >
-                        Must be a valid email address
-                    </p>
+                {errors.email &&
+                    <p role='alert' className={styles.inputError}>{errors.email.message}</p>
                 }
             </fieldset>
 
-            <fieldset
-                data-error={errors.password ? 'true' : 'false'}
-                className={styles.inputFieldset}
-            >
+            <fieldset data-error={errors.password ? 'true' : 'false'} className={styles.inputFieldset}>
                 <label htmlFor={'password' + inputID} className={styles.inputLabel}>Password:</label>
                 <input
                     {...register('password', {
-                        required: true,
+                        required: 'Required',
+                        minLength: {
+                            value: 8,
+                            message: 'Must be at least 8 characters',
+                        },
                         pattern: {
-                            value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+                            value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{1,}$/,
                             message: 'Must contain at least 1 number, uppercase letter, and lowercase letter',
                         }
                     })}
@@ -131,33 +107,21 @@ export const SignupForm = () => {
                     id={'password' + inputID}
                     className={styles.input}
                 />
-
-                {!watchPassword &&
-                    <p className={styles.inputError} >
-                        Must be 8 or more characters and contain at least 1 number, lowercase letter, and uppercase letter
-                    </p>
-                }
-                {watchPassword &&
-                    <p
-                        data-error={errors.password ? 'true' : 'false'}
-                        role={errors.password ? 'alert' : ''}
-                        className={styles.inputError}
-                    >
-                        Must be 8 or more characters and contain at least 1 number, lowercase letter, and uppercase letter
-                    </p>
+                {errors.password &&
+                    <p role='alert' className={styles.inputError}>{errors.password.message}</p>
                 }
             </fieldset>
 
-            <fieldset
-                data-error={errors.confirmPassword ? 'true' : 'false'}
-                className={styles.inputFieldset}
-            >
+            <fieldset data-error={errors.confirmPassword ? 'true' : 'false'} className={styles.inputFieldset} >
                 <label htmlFor={'confirmPassword' + inputID} className={styles.inputLabel}>Confirm Password:</label>
                 <input
                     {...register('confirmPassword', {
-                        required: 'Please enter confirm password',
+                        required: 'Required',
                         validate: (value, formValues) => {
-                            return value === formValues.password;
+                            return (value === formValues.password
+                                ? true
+                                : 'Must match password'
+                            );
                         },
                     })}
                     type='password'
@@ -165,20 +129,8 @@ export const SignupForm = () => {
                     id={'confirmPassword' + inputID}
                     className={styles.input}
                 />
-
-                {!watchConfirmPassword &&
-                    <p className={styles.inputError}>
-                        Must match password
-                    </p>
-                }
-                {watchConfirmPassword &&
-                    <p
-                        data-error={errors.confirmPassword ? 'true' : 'false'}
-                        role={errors.confirmPassword ? 'alert' : ''}
-                        className={styles.inputError}
-                    >
-                        Must match password
-                    </p>
+                {errors.confirmPassword &&
+                    <p role='alert' className={styles.inputError}>{errors.confirmPassword.message}</p>
                 }
             </fieldset>
 
