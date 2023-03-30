44a19439a0fb244bbf9c67f8054ad1b9245f151e
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-30 11:36:46 -0400

    feat: create confirm email page for updating user email

---
+++ src/pages/confirm-email.tsx
@@ -0,0 +1,42 @@
+
+import styles from '@/styles/ConfirmEmailPage.module.css'
+
+import Head from 'next/head'
+import { useSearchParams } from 'next/navigation'
+
+import { Header } from '@/components/header'
+
+import { Footer } from '@/components/footer'
+
+export default function ConfirmEmailPage() {
+
+    const searchParameters = useSearchParams();
+
+    const confirmationURL = searchParameters.get('confirmation');
+
+    return (
+        <>
+            <Head>
+                <title>Done</title>
+                <meta name='description' content='Get stuff done.' />
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+                <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+                <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+                <link rel='manifest' href='/site.webmanifest' />
+            </Head>
+
+            <div className={styles.page}>
+                <Header useSession={false} />
+
+                <main className={styles.main}>
+                    {confirmationURL &&
+                        <a href={confirmationURL} className={styles.link}>Confirm New Email</a>
+                    }
+                </main>
+
+                <Footer />
+            </div>
+        </>
+    );
+};
